28777aeb06b565a9c8e89355f95d833257d35079
NumdyPixe1 <numdao123@gmail.com>
2025-07-06 12:53:36 +0700

    ++

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,38 +1,14 @@
-"use server"
-import { NextResponse,NextRequest } from "next/server";
-const nodemailer = require("nodemailer")
+// "use server"
+// import { NextResponse,NextRequest } from "next/server";
+// const nodemailer = require("nodemailer")
  
 
-export async function POST(req: NextRequest) {
-  try {
-    const body = await req.json();
-    const { name, email, message } = body;
+// export async function POST(req: NextRequest) {
+//   try {
+//     const body = await req.json();
+//     const { name, email, message } = body;
 
-  }
-  catch{}
-}
-    //     // ✅ สร้าง transporter
-//     const transporter = nodemailer.createTransport({
-//       service: "gmail",
-//       auth: {
-//         user: process.env.SMTP_EMAIL,         // อีเมลผู้ส่ง (Gmail)
-//         pass: process.env.SMTP_PASSWORD,      // ต้องใช้ App Password (ไม่ใช่รหัสผ่านจริง)
-//       },
-//     });
-
-//     // ✅ ส่งอีเมล
-//     const mailOptions = {
-//       from: `"${name}" <${email}>`,
-//       to: process.env.SMTP_EMAIL, // ส่งหาตัวเอง
-//       subject: "Contact from portfolio site",
-//       text: message,
-//     };
-
-//     await transporter.sendMail(mailOptions);
-
-//     return NextResponse.json({ success: true, message: "Email sent successfully" });
-//   } catch (err) {
-//     console.error("Error sending email:", err);
-//     return NextResponse.json({ success: false, message: "Failed to send email" }, { status: 500 });
 //   }
+//   catch{}
 // }
+ 
